dc754ebce16c073afe1489d7f37917c6a96a03d2
Morteza Bandi <m_bandi@modernisc.com>
2019-12-23 17:28:53 +0330

    Initial commit

---
+++ src/main/java/com/morteza/Spring_Application.java
@@ -0,0 +1,12 @@
+package com.morteza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring_Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring_Application.class, args);
+    }
+}
